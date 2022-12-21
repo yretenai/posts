1c6bafa3a00fa3b20fffcc7b23e31310d80a1ad3
Naomi Ahmed <yretenai@gmail.com>
2022-12-21 22:58:56 +0000

    format things a bit

--- docs/markdown/00-test.md
+++ docs/markdown/00-test.md
@@ -1,3 +1,9 @@
+---
+title: this is a test
+short: this is just a test page
+date: 2022-12-21 10:41 PM
+---
+
 # this is a test
 
-hello
+hello, this is just a test page
--- generate-html.py
+++ generate-html.py
@@ -1,34 +1,28 @@
-from markdown import markdown
+from markdown import Markdown
 from glob import iglob
 from codecs import open as cope
+from datetime import datetime
 from os.path import basename, splitext
 
-HEADER = '\n<!-- could not read header -->\n'
-FOOTER = '\n<!-- could not read footer -->\n'
+with cope('post.html', 'r', 'utf8') as post_template:
+    POST_TEMPLATE = post_template.read().strip().replace('\r\n', '\n') + '\n'
 
-try:
-    with cope('__head.html', 'r', 'utf8') as header:
-        HEADER = header.read().replace('\r\n', '\n') + '\n'
-except: pass
-
-try:
-    with cope('__foot.html', 'r', 'utf8') as footer:
-        FOOTER = '\n' + footer.read().replace('\r\n', '\n')
-except: pass
+with cope('index.html', 'r', 'utf8') as index_template:
+    INDEX_TEMPLATE = index_template.read().strip().replace('\r\n', '\n') + '\n'
 
 with cope('docs/index.html', 'w', 'utf8') as index:
-    index.writelines([HEADER, '\n<ol>\n'])
-
+    index_lines = ''
     for md_file in iglob('docs/markdown/*.md'):
         with cope(md_file, 'r', 'utf8') as md:
-            md_text = md.read().strip()
-            title = md_text.split('\n', 1)[0][1:].strip()
-            text = markdown(md_text)
+            markdown = Markdown(extensions=['meta'])
+            text = markdown.convert(md.read().strip())
+            meta = markdown.Meta
             name = splitext(basename(md_file))[0]
-            target = '{name}.html'.format(name = name)
-            print(target)
-            with cope(target, 'w') as html:
-                html.writelines([HEADER, '<a href="index.html">back</a> - <a href="markdown/{name}.md">markdown</a>\n\n'.format(name = name), text, FOOTER])
-                index.writelines(['<a href="{url}">{title}</a>\n'.format(title = title, url = target)])
+            target = '{name}.html'.format(name=name)
+            index_lines += '<a href="{url}">{title}</a>\n'.format(title=meta['title'][0], url=target)
+            time = datetime.strptime(meta['date'][0], '%Y-%m-%d %I:%M %p')
 
-    index.writelines(['</ol>', FOOTER])
+            print(target)
+            with cope('docs/{url}'.format(url=target), 'w') as html:
+                html.write(POST_TEMPLATE.format(title=meta['title'][0], short=meta['short'][0], url=target, time=time, isotime=time.isoformat(), body=text))
+    index.write(INDEX_TEMPLATE.format(body=index_lines))
