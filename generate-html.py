267708032b9d465bb94d4c568b85053d1b71629f
Ada <yretenai@gmail.com>
2024-10-17 13:23:20 +0100

    feature: add rss, atom and jsonfeed

--- generate-html.py
+++ generate-html.py
@@ -4,6 +4,50 @@ from glob import glob
 from codecs import open as cope
 from datetime import datetime
 from os.path import basename, splitext
+from json import dumps as json_serialize
+from lxml.etree import tostring as xml_serialize
+from lxml.builder import E as elem
+from email.utils import format_datetime
+
+BLOG_NAME = "ada's blog"
+BLOG_ROOT = "https://chronovore.dev/posts"
+BLOG_WHOAMI = "Ada"
+BLOG_DESCRIPTION = "just a collection of thoughts~"
+BLOG_ID = "feed:chronovore:root"
+BLOG_POST_ID = "feed:chronovore:"
+
+atom_feed = [
+    elem.title(BLOG_NAME),
+    elem.link(href=BLOG_ROOT),
+    elem.updated(datetime.now().isoformat()),
+    elem.author(elem.name(BLOG_WHOAMI)),
+    elem.generator("pumpkin"),
+    elem.language("en"),
+    elem.id(BLOG_ID)
+]
+
+rss_feed = [
+    elem.title(BLOG_NAME),
+    elem.link(href=BLOG_ROOT),
+    elem.description(BLOG_DESCRIPTION),
+    elem.lastBuildDate(format_datetime(datetime.now())),
+    elem.generator("pumpkin"),
+    elem.language("en")
+]
+
+
+json_feed = []
+json_root = {
+    "version": "https://jsonfeed.org/version/1.1",
+    "title": BLOG_NAME,
+    "description": BLOG_DESCRIPTION,
+    "home_page_url": BLOG_ROOT,
+    "feed_url": f"{BLOG_ROOT}/feed.json",
+    "authors": [{"name": BLOG_WHOAMI}],
+    "generator": "pumpkin",
+    "language": "en",
+    "items": json_feed
+}
 
 with cope('post.html', 'r', 'utf8') as post_template:
     POST_TEMPLATE = post_template.read().strip().replace('\r\n', '\n') + '\n'
@@ -23,17 +67,63 @@ with cope('docs/index.html', 'w', 'utf8') as index:
             if not 'title' in meta:
                 continue
 
+            date = meta['date']
+            upd_date = meta['updated'] if 'updated' in meta else date
+            pub_date_t = datetime.strptime(date[0], '%Y-%m-%d %I:%M %p')
+            upd_date_t = datetime.strptime(upd_date[0], '%Y-%m-%d %I:%M %p')
+            pub_date_iso = pub_date_t.isoformat()
+            upd_date_iso = upd_date_t.isoformat()
+
             folder = 'private/'
             rel = '../'
             if 'private' not in meta:
-                index_lines += '<li><a href="{url}.html">{title}</a></li>\n'.format(title=meta['title'][0], url=name)
+                index_lines += f'<li><a href="{name}.html">{meta['title'][0]}</a></li>\n'
                 folder = ''
                 rel = ''
 
-            date = meta['date']
-            upd_date = meta['updated'] if 'updated' in meta else date
+                atom_feed.append(
+                    elem.entry(
+                        elem.title(meta['title'][0]),
+                        elem.link(href=f"{BLOG_ROOT}/{name}.html"),
+                        elem.updated(upd_date_iso),
+                        elem.published(pub_date_iso),
+                        elem.summary(meta['short'][0]),
+                        elem.id(BLOG_POST_ID + name)
+                    )
+                )
+
+                rss_feed.append(
+                    elem.item(
+                        elem.title(meta['title'][0]),
+                        elem.link(href=f"{BLOG_ROOT}/{name}.html"),
+                        elem.pubDate(format_datetime(pub_date_t)),
+                        elem.description(meta['short'][0]),
+                        elem.author(BLOG_WHOAMI),
+                        elem.guid(BLOG_POST_ID + name)
+                    )
+                )
+
+                json_feed.append({
+                    "title": meta['title'][0],
+                    "url": f"{BLOG_ROOT}/{name}.html",
+                    "id": BLOG_POST_ID + name,
+                    "summary": meta['short'][0],
+                    "date_published": pub_date_iso,
+                    "date_modified": upd_date_iso,
+                })
 
             print(name)
-            with cope('docs/{folder}{url}.html'.format(folder=folder, url=name), 'w', 'utf8') as html:
-                html.write(POST_TEMPLATE.format(title=meta['title'][0], short=meta['short'][0], url=name, time=upd_date[0], isotime=datetime.strptime(upd_date[0], '%Y-%m-%d %I:%M %p').isoformat(), pub_time=date[0], pub_isotime=datetime.strptime(date[0], '%Y-%m-%d %I:%M %p').isoformat(), body=text, rel=rel))
+            with cope(f'docs/{folder}{name}.html', 'w', 'utf8') as html:
+                html.write(POST_TEMPLATE.format(title=meta['title'][0], short=meta['short'][0], url=name, time=upd_date[0], isotime=upd_date_iso, pub_time=date[0], pub_isotime=pub_date_iso, body=text, rel=rel))
     index.write(INDEX_TEMPLATE.format(body=index_lines))
+
+atom = elem.feed(*atom_feed, xmlns="http://www.w3.org/2005/Atom")
+with cope('docs/feed.atom', 'wb') as atom_file:
+    atom_file.write(xml_serialize(atom, pretty_print=True, xml_declaration=True, encoding='utf8'))
+
+rss = elem.rss(elem.channel(*rss_feed, version="2.0"))
+with cope('docs/feed.rss', 'wb') as rss_file:
+    rss_file.write(xml_serialize(rss, pretty_print=True, xml_declaration=True, encoding='utf8'))
+
+with cope('docs/feed.json', 'w') as json_file:
+    json_file.write(json_serialize(json_root, indent=2)+'\n')
